b19cdbfc433b16c34c5c58d204c5dfdf67fbfc69
Adam Al-Qawasmeh <82409325+adamqaw@users.noreply.github.com>
2022-04-19 01:34:44 -0400

    Update wrd.py

--- wrd.py
+++ wrd.py
@@ -1,7 +1,12 @@
+# QUICK SOLVER FOR TESTING
+# In this example, word[0] and word[2] are uncertain, word[1], word[3], word[4] are green.
+
+
 word = list('*O*ER')
-letters = list('qeryuiopfhjkzxb'.upper())
+letters = list('qeryuiopfhjkzxb'.upper()) 
 
 with open('words.txt', mode='w') as data:
+    
     for i in range(len(letters)):
         nw = word.copy()
         nw[0] = letters[i]
